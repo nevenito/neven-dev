8fbcee0b044bc73e1e88e8addc9f5c5de18a05fd
nevenito <neven.zdelar@gmail.com>
2024-04-22 20:32:49 +0200

    fix: mall context build error

--- app/mall/_hooks/useMallContext.tsx
+++ app/mall/_hooks/useMallContext.tsx
@@ -1,5 +1,44 @@
-import { useContext } from "react";
-import { MallContext } from "../page";
+import { createContext, useContext } from "react";
+import { MallAction } from "./useMallReducer";
+
+// Create MallContext
+type MallContext = {
+  width: 2 | 4 | 6;
+  floors: number;
+  undergroundFloors: number;
+  groundGridRow: number;
+  getFloorPosition: (floor: number) => {
+    gridRowStart: number;
+    gridRowEnd: number;
+    gridColumnStart: number;
+    gridColumnEnd: number;
+  };
+  getWidthPosition: () => {
+    gridColumnStart: number;
+    gridColumnEnd: number;
+  };
+  dispatch: React.Dispatch<MallAction>;
+};
+
+const GROUND_LEVEL = 8;
+
+export const defaultContextValue: MallContext = {
+  width: 6,
+  floors: 3,
+  groundGridRow: GROUND_LEVEL,
+  undergroundFloors: 1,
+  getFloorPosition: () => {
+    throw new Error("getFloorPosition not implemented");
+  },
+  getWidthPosition: () => {
+    throw new Error("getWidthPosition not implemented");
+  },
+  dispatch: () => {
+    throw new Error("dispatch not implemented");
+  },
+};
+
+export const MallContext = createContext<MallContext>(defaultContextValue);
 
 const useMallContext = () => {
   const context = useContext(MallContext);
--- app/mall/page.tsx
+++ app/mall/page.tsx
@@ -2,45 +2,9 @@
 import { createContext, useMemo, useReducer, useState } from "react";
 import Base from "./_components/Base";
 import useMallReducer, { MallAction } from "./_hooks/useMallReducer";
-
-// Create MallContext
-type MallContext = {
-  width: 2 | 4 | 6;
-  floors: number;
-  undergroundFloors: number;
-  groundGridRow: number;
-  getFloorPosition: (floor: number) => {
-    gridRowStart: number;
-    gridRowEnd: number;
-    gridColumnStart: number;
-    gridColumnEnd: number;
-  };
-  getWidthPosition: () => {
-    gridColumnStart: number;
-    gridColumnEnd: number;
-  };
-  dispatch: React.Dispatch<MallAction>;
-};
+import { MallContext, defaultContextValue } from "./_hooks/useMallContext";
 
 const GROUND_LEVEL = 8;
-
-const defaultContextValue: MallContext = {
-  width: 6,
-  floors: 3,
-  groundGridRow: GROUND_LEVEL,
-  undergroundFloors: 1,
-  getFloorPosition: () => {
-    throw new Error("getFloorPosition not implemented");
-  },
-  getWidthPosition: () => {
-    throw new Error("getWidthPosition not implemented");
-  },
-  dispatch: () => {
-    throw new Error("dispatch not implemented");
-  },
-};
-
-export const MallContext = createContext<MallContext>(defaultContextValue);
 
 const Mall = () => {
   const [state, dispatch] = useMallReducer(defaultContextValue);
