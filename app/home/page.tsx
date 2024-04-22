1306a530224bdccd827ec5caddcd2eba4ae26664
nevenito <neven.zdelar@gmail.com>
2024-04-22 20:33:25 +0200

    feat: add shad

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,22 +1,22 @@
 import { Tranquiluxe, Velustro } from "uvcanvas";
 import ProjectsSection from "./_components/ProjectsSection";
 import Link from "next/link";
-import Button from "../_shared/ui/Button";
+import { Button } from "../_shared/ui/Button";
 
 export default function Home() {
   return (
     <main className="min-h-max">
       <div className="h-screen">
-        <div className="h-full p-20 relative rounded">
+        <div className="relative h-full rounded p-20">
           <Velustro />
-          <h1 className="absolute flex w-full justify-center items-center h-full top-0 left-0 text-9xl text-gray-100 font-semibold">
+          <h1 className="absolute left-0 top-0 flex h-full w-full items-center justify-center text-9xl font-semibold text-gray-100">
             Neven Zdelar
           </h1>
         </div>
       </div>
 
       <ProjectsSection />
-      <section className="mx-20 rounded mb-20 grid justify-center text-center gap-4 py-10 bg-green-500">
+      <section className="mx-20 mb-20 grid justify-center gap-4 rounded bg-green-500 py-10 text-center">
         <h2>Try Mall Builder 3000!</h2>
         <Link href="/mall">
           <Button>Go to Mall Builder 3000</Button>
