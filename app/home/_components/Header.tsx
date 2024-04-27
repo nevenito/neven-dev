984ca4edc370946f767dc8d04e516bfbe84fc46b
nevenito <neven.zdelar@gmail.com>
2024-04-27 21:13:03 +0200

    style: properly center nav

--- app/home/_components/Header.tsx
+++ app/home/_components/Header.tsx
@@ -4,16 +4,13 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-[99] flex h-[8vh] items-center bg-background px-standard text-lg font-medium shadow">
-      <Link
-        href="#hero"
-        className="group mr-auto flex items-center gap-3 text-primary"
-      >
+    <header className="sticky top-0 z-[99] grid h-[8vh] grid-cols-3 items-center  bg-background px-standard text-lg font-medium shadow">
+      <Link href="#hero" className="group flex items-center gap-3 text-primary">
         <Chequy className="size-5 transition group-hover:rotate-90" />
         <h1>Neven Zdelar</h1>
       </Link>
       <nav>
-        <ul className="flex gap-16 text-white">
+        <ul className="flex justify-center gap-16 text-white">
           <Link className="hover:opacity-90" href="#about">
             About
           </Link>
